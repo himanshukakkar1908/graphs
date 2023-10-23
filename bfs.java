442fc3198e76aa44a55f122cd9fd3f71694bfeed
It-Is-Ishank <102756532+It-Is-Ishank@users.noreply.github.com>
2023-10-23 16:29:52 +0530

    Update bfs.java with comments
    added comments to better understand the functioning of the code

--- bfs.java
+++ bfs.java
@@ -28,81 +28,88 @@ import java.util.Scanner;
 
 public class bfs {
 
-    static class Edge{
-        int src;
-        int nbr;
-        int wt;
-        Edge(int src,int nbr,int wt){
-            this.src=src;
-            this.nbr=nbr;
-            this.wt=wt;
+    // Define a class to represent edges in the graph
+    static class Edge {
+        int src;  // Source vertex
+        int nbr;  // Neighbor vertex
+        int wt;   // Weight of the edge
+
+        Edge(int src, int nbr, int wt) {
+            this.src = src;
+            this.nbr = nbr;
+            this.wt = wt;
         }
     }
 
-    static class Pair{
-        String psf;
-        int vertice;
-        Pair(int vertice,String psf){
-            this.vertice=vertice;
-            this.psf=psf;
+    // Define a class to represent pairs of vertices and their paths
+    static class Pair {
+        String psf;  // Path so far
+        int vertice; // Current vertex
+
+        Pair(int vertice, String psf) {
+            this.vertice = vertice;
+            this.psf = psf;
         }
     }
 
     public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
 
-        Scanner scn=new Scanner(System.in);
+        // Read the number of vertices and edges in the graph
+        int vertices = scn.nextInt();
+        int edges = scn.nextInt();
 
-        int vertices=scn.nextInt();
-        int edges=scn.nextInt();
+        // Create an array of ArrayList to represent the graph
+        ArrayList<Edge>[] graph = new ArrayList[vertices];
 
-        ArrayList<Edge>[] graph=new ArrayList[vertices];
-
-        for(int i=0;i<graph.length;i++){
-            graph[i]=new ArrayList<>();
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
         }
 
-        for(int i=1;i<=edges;i++){
-            int src=scn.nextInt();
-            int nbr=scn.nextInt();
-            int wt= scn.nextInt();
-            Edge e=new Edge(src,nbr,wt);
+        // Read the edges and construct the graph
+        for (int i = 1; i <= edges; i++) {
+            int src = scn.nextInt();
+            int nbr = scn.nextInt();
+            int wt = scn.nextInt();
+            Edge e = new Edge(src, nbr, wt);
             graph[src].add(e);
-            Edge e_=new Edge(nbr,src,wt);
+            Edge e_ = new Edge(nbr, src, wt);
             graph[nbr].add(e_);
         }
 
-        boolean[] visited=new boolean[vertices];
+        boolean[] visited = new boolean[vertices];
 
-        int src=scn.nextInt();
+        int src = scn.nextInt();
 
-        bfs_function(graph,src);
+        bfs_function(graph, src);
     }
 
+    // Function to perform Breadth-First Search (BFS) on the graph
+    public static void bfs_function(ArrayList<Edge>[] graph, int src) {
 
+        // Create a queue to process vertices in BFS order
+        Queue<Pair> queue = new ArrayDeque<>();
+        boolean[] visited = new boolean[graph.length];
 
-    public static void bfs_function(ArrayList<Edge>[] graph,int src){
+        // Start with the source vertex and mark it as visited
+        queue.add(new Pair(src, "" + src));
+        visited[src] = true;
 
-        Queue<Pair> queue=new ArrayDeque<>();
-        boolean[] visited=new boolean[graph.length];
+        while (!queue.isEmpty()) {
+            // Dequeue a vertex and its path from the queue
+            Pair p = queue.remove();
+            System.out.println(p.vertice + "@" + p.psf); // Print the vertex and its path
 
-        queue.add(new Pair(src,""+src));
-        visited[src]=true;
-
-        while (queue.size()!=0){
-
-            Pair p=queue.remove();
-            System.out.println(p.vertice+"@"+p.psf);
-
-            for(Edge e:graph[p.vertice]){
-                if(!visited[e.nbr]){
-                    Pair pe=new Pair(e.nbr, p.psf+e.nbr);
+            // Explore its neighbors
+            for (Edge e : graph[p.vertice]) {
+                if (!visited[e.nbr]) {
+                    // If a neighbor is not visited, add it to the queue
+                    // with an updated path and mark it as visited
+                    Pair pe = new Pair(e.nbr, p.psf + e.nbr);
                     queue.add(pe);
-                    visited[e.nbr]=true;
+                    visited[e.nbr] = true;
                 }
             }
-
         }
-
     }
-
 }
